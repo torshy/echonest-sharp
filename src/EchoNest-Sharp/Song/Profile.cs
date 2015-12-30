9d82aa612fb29c6c42782a365f098d27b91f347b
Vijay Santhanam <vijay@pandahq.com.au>
2015-12-30 15:08:26 +1100

    Add Profile service + related classes

---
+++ src/EchoNest-Sharp/Song/Profile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoNest.Song
+{
+    public class Profile : EchoNestService
+    {
+        #region Fields
+
+        private const string Url = "song/profile";
+
+        #endregion Fields
+
+        #region Methods
+
+        public SearchResponse Execute(ProfileArgument argument)
+        {
+            argument.ApiKey = ApiKey;
+            argument.BaseUrl = Url;
+
+            return Execute<SearchResponse>(argument.ToString());
+        }
+
+        public Task<SearchResponse> ExecuteAsync(ProfileArgument argument)
+        {
+            argument.ApiKey = ApiKey;
+            argument.BaseUrl = Url;
+
+            return ExecuteAsync<SearchResponse>(argument.ToString());
+        }
+
+        #endregion Methods
+    }
+}
---
+++ src/EchoNest-Sharp/Song/ProfileResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace EchoNest.Song
+{
+    [DataContract]
+    public class ProfileResponse : Response
+    {
+        [DataMember(Name = "songs")]
+        public List<SongBucketItem> Songs { get; set; }
+    }
+}
